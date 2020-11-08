7a00cc8c2b7214ac0a4130a8c69f2af4e9fa3f35
jwmccowan <jwmccowan@gmail.com>
2020-11-09 02:21:30 +1100

    remove unused app controller and app service

--- app/api/src/app.module.ts
+++ app/api/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
@@ -8,7 +6,5 @@ import { GQLModule } from './graphql/graphql.module';
 
 @Module({
   imports: [AuthModule, GQLModule, DatabaseModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
